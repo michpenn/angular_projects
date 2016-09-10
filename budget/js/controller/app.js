b4e1301a95f247cc532650a69420254f56e89400
michpenn <michpenn91@gmail.com>
2016-09-09 23:20:13 -0700

    http request to taxee v2 now works

--- budget/js/controller/app.js
+++ budget/js/controller/app.js
@@ -64,7 +64,7 @@
 
     }]);
 
-    app.controller('expensesController', ['$scope', 'Expenses','Expense', function ($scope, Expenses, Expense) {
+    app.controller('expensesController', ['$scope', 'Expenses', 'Expense', function ($scope, Expenses, Expense) {
         var self = this;
         self.title = 'Expenses';
         self.expenses = Expenses.expenses;
@@ -153,7 +153,7 @@
         service.updateExpense = function (expense) {
             expense.totalCost = expense.cost * expense.frequency;
             var toUpdate = service.getById(expense.id);
-            if(expense.id = toUpdate.id) {
+            if (expense.id = toUpdate.id) {
                 _.extend(toUpdate, expense);
                 service.getExpenses();
             }
@@ -189,12 +189,12 @@
             return cost * frequency;
         };
 
-        service.sumTotalExpenses = function(){
-            if(service.expenses.length === 0){
+        service.sumTotalExpenses = function () {
+            if (service.expenses.length === 0) {
                 return 0;
             }
             else {
-                return _.reduce(service.expenses, function(memo, num) {
+                return _.reduce(service.expenses, function (memo, num) {
                     return memo + num.totalCost;
                 }, 0);
 
@@ -220,7 +220,7 @@
             service.expenseObjChanged();
         };
 
-        service.edit = function(expense){
+        service.edit = function (expense) {
             service.expenseObj = _.clone(Expenses.getById(expense.id));
             service.expenseObjChanged();
 
@@ -254,7 +254,7 @@
         return service;
     });
 
-    app.factory('Taxes', ['$http', '$q', function ($http, $q) {
+    app.factory('Taxes', ['$http', '$q', '$interpolate', function ($http, $q, $interpolate) {
         var service = {};
         service.filingStatusOptions = [
             {name: 'single', id: 1, parameter: 'single'},
@@ -270,69 +270,92 @@
             var config = {
                 'async': true,
                 'crossDomain': true,
-                method: 'POST',
-                url: 'http://taxee.io/api/v1/calculate/2016',
-                data: {
-                    'filing_status': salary_data.filingStatus,
-                    'pay_periods': 1,
-                    'pay_rate': salary_data.initialSalary,
-                    'state': salary_data.state
-                },
                 headers: {
-                    "cache-control": "no-cache",
+                    "authorization": "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJBUElfS0VZX01BTkFHRVIiLCJodHRwOi8vdGF4ZWUuaW8vdXNlcl9pZCI6IjU3ZDM5MjU3Y2RiYjc1MmM5OTkyNDVmOSIsImh0dHA6Ly90YXhlZS5pby9zY29wZXMiOlsiYXBpIl0sImlhdCI6MTQ3MzQ4MzM1MX0.NTja_sCVYtIGsmmeO8IJSs6lAr8pGLmfn3yf9ih9uI0",
                     'content-type': 'application/x-www-form-urlencoded'
 
                 }
             };
-            $http.jsonp('http://taxee.io/api/v1/calculate/2016', config)
+            var data = {
+                'filing_status': salary_data.filingStatus,
+                'pay_periods': 1,
+                'pay_rate': salary_data.initialSalary,
+                'state': salary_data.state
+            };
+
+            var dataFunc = $interpolate('state={{salary_data.state}}&filing_status={{salary_data.filingStatus}}&pay_periods=1&pay_rate={{salary_data.initialSalary}}');
+            var url_encoded_string = dataFunc({salary_data:salary_data});
+            console.log('url:', url_encoded_string);
+
+
+            $http.post('https://taxee.io/api/v2/calculate/2016', url_encoded_string,config)
                 .then(function (res) {
                     console.log('res:', res);
                 });
 
-            //$.ajax(config).done(function(response){console.log('res:', response);});
-            //$http(config).then(function(res){
-            //  console.log('success:', res)},
-            //function(res){console.log('error:', res)});
         };
         return service;
 
     }]);
 
-    app.directive('budgetSheet', function(){
+    app.directive('budgetSheet', function () {
         return {
             restrict: 'E',
             templateUrl: 'budget-sheet.html'
         }
     });
 
-    app.directive('salaryForm', function(){
+    app.directive('salaryForm', function () {
         return {
             restrict: 'E',
             templateUrl: 'salary-form.html'
         }
     });
 
-    app.directive('expensesTable', function(){
+    app.directive('expensesTable', function () {
         return {
             restrict: 'E',
-            templateUrl: 'expenses-table.html'
+            templateUrl: 'expenses-table.html',
+            controller: 'expensesController',
+            require: 'expensesTable',
+            link: linkFn
+        };
+        function linkFn($scope, element, attr, controller) {
+
         }
     });
 
-    app.directive('expenseForm', function(){
+    app.directive('expenseForm', function () {
         return {
             restrict: 'E',
-            templateUrl: 'expense-form.html'
+            templateUrl: 'expense-form.html',
+            controller: 'expenseController',
+            require: 'expenseForm',
+            //controllerAs: 'expenses',
+            link: linkFn
+        };
+        function linkFn($scope, element, attr, controller) {
+
         }
     });
-    app.directive('appTitle', function(){
+    app.directive('appTitle', function () {
         return {
             restrict: 'E',
             templateUrl: 'app-title.html'
         }
     });
-
-
+    app.directive('addExpense', function () {
+        return {
+            restrict: 'E',
+            templateUrl: 'add-expense.html'
+        }
+    });
+    app.directive('expensesSection', function () {
+        return {
+            restrict: 'E',
+            templateUrl: 'expenses-section.html'
+        }
+    });
 
 
 })();
