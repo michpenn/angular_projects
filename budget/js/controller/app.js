bba8e34e4696e29a1afaace4095d7021712aec5b
michpenn <michpenn91@gmail.com>
2016-10-04 18:57:10 -0700

    handles editing the salary form

--- budget/js/controller/app.js
+++ budget/js/controller/app.js
@@ -45,14 +45,42 @@
         self.stateOptions = Taxes.stateOptions;
         self.state = null;
         self.salaryFormSubmitted = false;
+        self.salaryFormEdit = false;
         self.save = function (object) {
             self.salaryFormSubmitted = true;
             Salary.saveSalary(object.initialSalary);
             self.initialSalary = object.initialSalary;
             if (object.filingStatus) {
+                self.filingStatus = object.filingStatus;
+                self.state = object.state;
                 Taxes.calculateTaxes(object);
             }
             //console.log('save object:', object);
+        };
+        self.editClicked = function(){
+            self.salaryFormEdit = true;
+        };
+        self.updateSalary = function(object){
+
+            if((object.initialSalary != self.initialSalary) || (object.state != self.state) || ((object.filingStatus != self.filingStatus) && (object.filingStatus != null) && ((self.filingStatus != null)))){
+                if(object.initialSalary != self.initialSalary) {
+                    Salary.saveSalary(object.initialSalary);
+                }
+                Taxes.updateTaxes(object);
+
+            }
+            else if((object.filingStatus == null) && (self.filingStatus != null)) {
+                Taxes.removeTaxExpenses();
+            }
+
+            else if((self.filingStatus == null) && (object.filingStatus != null)) {
+                //add new tax expenses to the beginning of the array
+                Taxes.calculateTaxes(object);
+            }
+
+
+
+            self.salaryFormEdit = false;
         };
         self.edit = function (object) {
             Salary.saveSalary(object.initialSalary);
@@ -128,7 +156,7 @@
 
         self.edit = function () {
             Expense.edit();
-            console.log('self:', self);
+
         };
 
 
@@ -145,10 +173,24 @@
         service.totalCostExpenses = 0;
 
         service.addNewExpense = function (expense) {
-            console.log(expense);
+
+
             expense.id = service.getNewId();
             expense.totalCost = service.getTotalCost(expense.cost, expense.frequency);
-            service.expenses.push(expense);
+
+            if(expense.category == 'Taxes') {
+                var index = _.findLastIndex(service.expenses, {'category': 'Taxes'});
+                if(index == -1) {
+                    service.expenses.push(expense);
+                }
+                else {
+                    service.expenses.splice(index,0, expense);
+                }
+            }
+            else {
+                service.expenses.push(expense);
+            }
+
             service.getExpenses();
         };
         service.updateExpense = function (expense) {
@@ -160,6 +202,15 @@
             }
 
         };
+        service.findExpenseId = function(category,name) {
+
+            var expenseObj = _.find(service.expenses, function(expense) {
+                return((expense.category == category) && (expense.name == name));
+            });
+
+            return (expenseObj? expenseObj.id : false);
+        };
+
         service.getNewId = function () {
             if (service.expenses.length > 0) {
                 var id = _.max(service.expenses, function (expense) {
@@ -232,6 +283,7 @@
                 Expenses.updateExpense(expense);
             }
             else {
+                expense.manuallyAdded = true;
                 Expenses.addNewExpense(expense);
             }
         };
@@ -260,6 +312,7 @@
         service.filingStatusOptions = filingStatusOptions;
         service.pay_periods = 1;
         service.stateOptions = stateOptions;
+
         service.calculateTaxes = function (salary_data) {
             console.log('salary data:', salary_data);
             var config = {
@@ -292,11 +345,70 @@
                         obj.name = taxType;
                         obj.frequency = 1;
                         obj.cost = taxes[taxType]['amount'];
+                        obj.manuallyAdded = false;
                         Expenses.addNewExpense(obj);
+                    }
+                });
+
+        };
+
+        service.updateTaxes = function(salary_data){
+            //console.log('salary data update taxes:', salary_data);
+            var config = {
+                'async': true,
+                'crossDomain': true,
+                headers: {
+                    "authorization": "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJBUElfS0VZX01BTkFHRVIiLCJodHRwOi8vdGF4ZWUuaW8vdXNlcl9pZCI6IjU3ZDM5MjU3Y2RiYjc1MmM5OTkyNDVmOSIsImh0dHA6Ly90YXhlZS5pby9zY29wZXMiOlsiYXBpIl0sImlhdCI6MTQ3MzQ4MzM1MX0.NTja_sCVYtIGsmmeO8IJSs6lAr8pGLmfn3yf9ih9uI0",
+                    'content-type': 'application/x-www-form-urlencoded'
+
+                }
+            };
+            var data = {
+                'filing_status': salary_data.filingStatus,
+                'pay_periods': 1,
+                'pay_rate': salary_data.initialSalary,
+                'state': salary_data.state
+            };
+
+            var dataFunc = $interpolate('state={{salary_data.state}}&filing_status={{salary_data.filingStatus}}&pay_periods=1&pay_rate={{salary_data.initialSalary}}');
+            var url_encoded_string = dataFunc({salary_data:salary_data});
+
+            $http.post('https://taxee.io/api/v2/calculate/2016', url_encoded_string,config)
+                .then(function (res) {
+                    var taxes = res.data.annual;
+                    for(var taxType in taxes) {
+                        var obj ={};
+                        obj.category = 'Taxes';
+                        obj.name = taxType;
+                        obj.frequency = 1;
+                        obj.cost = taxes[taxType]['amount'];
+                        obj.id = Expenses.findExpenseId('Taxes', taxType);
+                        obj.manuallyAdded = false;
+
+                        if(obj.id) {
+                            Expenses.updateExpense(obj);
+                        }
+                        else {
+                            Expenses.addNewExpense(obj);
+                            //console.log('need to add this:', obj);
+                        }
+
+
 
                     }
                 });
 
+
+
+
+        };
+
+        service.removeTaxExpenses = function(){
+            var removed_taxes = _.reject(Expenses.expenses, function(expense) {
+                return ((expense.category == 'Taxes') && (!expense.manuallyAdded));
+            });
+            Expenses.expenses = removed_taxes;
+            Expenses.getExpenses();
         };
         return service;
 
@@ -358,6 +470,31 @@
         return {
             restrict: 'E',
             templateUrl: 'expenses-section.html'
+        }
+    });
+
+    app.filter('capitalize', function() {
+        return function(input) {
+            if(!!input) {
+
+
+                var splitString = input.split(' ');
+                var stringPartsCaps = [];
+                for (var i = 0; i < splitString.length; i++) {
+                    if (!!splitString[i]) {
+                        stringPartsCaps.push(splitString[i].charAt(0).toUpperCase() + splitString[i].substr(1).toLowerCase());
+                    }
+                    else {
+                        stringPartsCaps.push('');
+                    }
+
+                }
+
+                return stringPartsCaps.join(' ');
+            }
+            else {
+                return '';
+            }
         }
     });
 
